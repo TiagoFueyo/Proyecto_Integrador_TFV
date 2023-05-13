bcdcc4e81811bfabca2726a780a4986b4f6bfcb1
TiagoFueyo <132405850+TiagoFueyo@users.noreply.github.com>
2023-05-12 23:16:40 -0300

    Dockerfile

---
+++ Proyecto_Integrador_TFV/Backend/src/main/java/com/portfolio/mgb/Interface/IPersonaService.java
@@ -0,0 +1,20 @@
+package com.portfolio.mgb.Interface;
+
+import com.portfolio.mgb.Entity.Persona;
+import java.util.List;
+
+
+
+public interface IPersonaService {
+    //Traer una lista de personas
+    public List<Persona> getPersona();
+    
+    //Guardar un objeto de tipo Persona
+    public void savePersona (Persona persona) ;
+    
+    //Eliminar un objeto pero lo buscamos por Id
+    public void deletePersona (Long id) ;
+    
+    //Buscar una persona por Id
+    public Persona findPersona (Long id) ;
+}
--- Proyecto_Integrador_TFV/Frontend/src/app/app.module.ts
+++ Proyecto_Integrador_TFV/Frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -13,6 +13,15 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HysComponent } from './components/hys/hys.component';
 import { ProyectoComponent } from './components/proyecto/proyecto.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { interceptorProvider } from './service/interceptor-service';
+import { FormsModule } from '@angular/forms';
+import { NewExperienciaComponent } from './components/experiencia/new-experiencia/new-experiencia.component';
+import { EditExperienciaComponent } from './components/experiencia/edit-experiencia/edit-experiencia.component';
+import { NeweducacionComponent } from './components/educacion/neweducacion.component';
+import { EditeducacionComponent } from './components/educacion/editeducacion.component';
 
 @NgModule({
   declarations: [
@@ -25,14 +34,26 @@ import { FooterComponent } from './components/footer/footer.component';
     EducacionComponent,
     HysComponent,
     ProyectoComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    LoginComponent,
+    NewExperienciaComponent,
+    EditExperienciaComponent,
+    NeweducacionComponent,
+    EditeducacionComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgCircleProgressModule.forRoot({})
+    FormsModule,
+    BrowserAnimationsModule,
+    NgCircleProgressModule.forRoot({}),
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    interceptorProvider
+  ],
   bootstrap: [AppComponent]
+  
 })
 export class AppModule { }
